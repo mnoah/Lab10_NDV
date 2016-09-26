0d6fc7de3f2984e4bdf8a3a6ffad10be0f275aaa
mnoah <noahvrazo@gmail.com>
2016-09-26 09:16:47 -0400

    submit

---
+++ Movies/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_NDV
+{
+    public class Movie
+    {
+        private  string MovieTitle = "";
+
+        private  string MovieCategory = "";
+
+        public string getTitle()
+        {
+            return MovieTitle;
+        }
+
+        public string getCategory()
+        {
+            return MovieCategory;
+        }
+
+        public Movie (string title, string category)
+        {
+                MovieTitle = title;
+                MovieCategory = category;
+        }
+
+            
+
+
+    }
+}
